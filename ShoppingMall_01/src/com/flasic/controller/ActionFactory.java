5e54deca11cf197eeb84560d62cac222bf1ee27a
suddn8606 <suddn8606@naver.com>
2017-01-23 15:50:22 +0900

    step1

---
+++ ShoppingMall_01/src/com/flasic/controller/ActionFactory.java
@@ -0,0 +1,22 @@
+package com.flasic.controller;
+
+import com.flasic.controller.action.Action;
+
+public class ActionFactory {
+	private static ActionFactory instance = new ActionFactory();
+	private ActionFactory() {
+		super();
+	}
+	public static ActionFactory getInstance() {
+		return instance;
+	}
+	  public Action getAction(String command) {
+		Action action =null;
+	    if (command.equals("index")) {
+	       
+	      }
+		
+		  return action;
+		  
+	  }
+}
--- ShoppingMall_01/src/com/flasic/controller/FlasicServlet.java
+++ ShoppingMall_01/src/com/flasic/controller/FlasicServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.flasic.controller.action.Action;
 
 /**
  * Servlet implementation class FlasicServlet
@@ -26,7 +27,11 @@ public class FlasicServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String command = request.getParameter("command");
+		System.out.println("FlasicServlet에서 요청을 받음을 확인 : " + command);
+	    ActionFactory af = ActionFactory.getInstance();
+	    Action action = af.getAction(command);
 	}
 
 	/**
